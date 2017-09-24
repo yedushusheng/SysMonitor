96c9330c0fdefb66ca90d6a4391648486159eb60
yedushusheng <yedushusheng@163.com>
2017-09-24 14:32:05 +0800

    add sysmonitor main

---
+++ SysMonitor.py
@@ -0,0 +1,29 @@
+from cpu import CPUMonitor
+from mem import MemMonitor
+from disk import DiskMonitor
+from net import NetMonitor
+from misc import MiscMonitor
+from temperature import TempMonitor
+from arch import ArchInfo
+
+if __name__ == '__main__':
+    cpu = CPUMonitor()
+    cpu.cpuInfo()
+    mem = MemMonitor()
+    mem.memInfo()
+    disk = DiskMonitor()
+    disk.diskInfo()
+    net = NetMonitor()
+    net.netInfo()
+    misc = MiscMonitor()
+    misc.userInfo()
+    misc.bootTime()
+    misc.Battery()
+    #TempMonitor.TempInfo(0)
+    arch = ArchInfo()
+    arch.SystemType()
+    arch.SystemInfo()
+    arch.PlatformInfo()
+
+
+
