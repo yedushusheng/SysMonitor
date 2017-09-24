27974764e3c31d170f55e3cc34a3b5feebaeedd0
yedushusheng <yedushusheng@163.com>
2017-09-24 14:28:00 +0800

    add net monitor

---
+++ net.py
@@ -0,0 +1,28 @@
+import psutil
+import time
+import netifaces
+
+class NetMonitor(object):
+    def __init__(self,pernic=True):
+        self.pernic = pernic
+
+    #获取网络信息
+    def netInfo(self):
+        print("网络信息".center(20,'*'))
+        #获取网络总IO信息
+        netTotalIO = psutil.net_io_counters()
+        print(type(netTotalIO))
+        print(netTotalIO)
+
+
+        #获取每个网络接口的IO信息
+        counter = psutil.net_io_counters(pernic=True)
+
+        #获取网络ip地址
+        netIP = psutil.net_if_addrs()
+        #获取网络状态
+        netStatus = psutil.net_if_stats()
+        #获取网络连接种类
+        netConn = psutil.net_connections()
+
+
