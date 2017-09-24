0a9b87c91c50d79a548a10b10f16e50f533a3423
yedushusheng <yedushusheng@163.com>
2017-09-24 17:20:23 +0800

    增加内存监控信息输出

--- mem.py
+++ mem.py
@@ -6,7 +6,14 @@ class MemMonitor(object):
         print("内存信息".center(20,'*'))
         #获取虚拟内存大小
         memVirtual = psutil.virtual_memory()
+        print("虚拟内存:%s" %memVirtual)
         #获取交换分区大小
         memSwap = psutil.swap_memory()
+        print("交换分区:%s" %memSwap)
         #获取实际物理内存
         memPhysical = psutil._TOTAL_PHYMEM
+        print("物理内存:%s" %memPhysical)
+
+if __name__ == '__main__':
+    mem = MemMonitor()
+    mem.memInfo()
