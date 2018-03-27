fe662804882a5004c4fa9f0a832aeb64c0e220ea
yedushusheng <yedushusheng@163.com>
2018-03-28 00:17:23 +0800

    修改显示菜单

--- menue.py
+++ menue.py
@@ -4,10 +4,15 @@ class MonitorMenue(object):
 
     time = MonitorTime().Time()
     def Menue(self):
-        print("系统监控平台".center('*',20))
-        print("监控内容:" %'\n')
-        print("CPU性能".ljust('-',5))
-        print("内存性能".ljust('-',5))
-        print("网络状态".ljust('-',5))
-        print("进程监控".ljust('-',5))
-        print("监控时间:%s".ljust('-',5) )
+        print("System Monitor platform".center('*',20))
+        print("Monitor item:" %'\n')
+        print("CPU information".ljust('-',5))
+        print("Mem information".ljust('-',5))
+        print("Net status".ljust('-',5))
+        print("Process status".ljust('-',5))
+        print("Monitor time:%s".ljust('-',5) )
+
+
+if __name__ == '__main__':
+    menue = MonitorMenue()
+    menue.Menue()
