ce164827578a213fafec39cab4b4023669de3359
yedushusheng <yedushusheng@163.com>
2018-03-27 23:11:54 +0800

    输出修改为英文

--- socket.py
+++ socket.py
@@ -27,8 +27,8 @@ class SockServer(object):
         while(True):
             clientsocket,addr = serversocket.accept()
 
-            print("连接地址:%s" % str(addr))
+            print("connected address:%s" % str(addr))
 
-            msg = "欢迎使用系统监控平台！" +"\r\n"
+            msg = "Welcome to SysMonitor utils!" +"\r\n"
             clientsocket.send(msg.encode('utf-8'))
             clientsocket.close()
