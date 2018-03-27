61404819a285a3c77f488aec3d073957bb52405a
yedushusheng <yedushusheng@163.com>
2018-03-27 23:14:15 +0800

    修改内存信息为英文输出

--- mem.py
+++ mem.py
@@ -3,16 +3,16 @@ import psutil
 class MemMonitor(object):
     #内存监控
     def memInfo(self):
-        print("内存信息".center(20,'*'))
+        print("Memory information".center(20,'*'))
         #获取虚拟内存大小
         memVirtual = psutil.virtual_memory()
-        print("虚拟内存:%s" %memVirtual)
+        print("Virt mem:%s" %memVirtual)
         #获取交换分区大小
         memSwap = psutil.swap_memory()
-        print("交换分区:%s" %memSwap)
+        print("Swap mem:%s" %memSwap)
         #获取实际物理内存
         memPhysical = psutil._TOTAL_PHYMEM
-        print("物理内存:%s" %memPhysical)
+        print("Phisical mem:%s" %memPhysical)
 
 if __name__ == '__main__':
     mem = MemMonitor()
